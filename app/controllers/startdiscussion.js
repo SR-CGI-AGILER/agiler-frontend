37a2af56e3dfed5888893a39b2e950368bad1217
Ankit-Mund <ankit7mund@gmail.com>
2018-10-18 18:21:54 +0530

    Rooms extracted fron db

--- app/controllers/startdiscussion.js
+++ app/controllers/startdiscussion.js
@@ -3,7 +3,7 @@ import Controller from '@ember/controller';
 export default Controller.extend({
     actions: {
         goToDiscussion(){
-            this.transitionToRoute('project-view')
+            this.transitionToRoute('discuss')
         }
     }
 });
