1ae2e170639dcaf1fbc1fe8c4b0b083911a2ccf1
Anirudha11 <anirudha.padhee@gmail.com>
2018-10-26 00:30:23 +0530

    modified project adapter

--- app/adapters/project.js
+++ app/adapters/project.js
@@ -1,60 +1,21 @@
-// import DS from 'ember-data';
-// import {inject as service} from '@ember/service';
-
-
-// export default DS.RESTAdapter.extend({
-//     session: service('session'),
-//     buildURL(modelName, id, snapshot, requestType, query){
-//         debugger
-//         if (query) {
-//             // console.log(query.assignTo,"gggg")
-//             return  `http://localhost:8000/api/v1/teams/${query.assignTo}/projects`;
-//         }else {
-//             let memberId =this.get('session').session.content.authenticated.userdata.id;
-//             console.log(memberId,"hgfchgdasghsagh")
-//             return `http://localhost:8000/api/v1/member/${memberId}/projects/`;
-//         }
-      
-//     }
-// })
-//     // urlForQuery (query, modelName) {
-//     //     // switch(modelName) {
-//     //         // case 'repo' :
-//     //         debugger
-//     //         return  `http://172.23.238.195:8000/api/v1/teams/${query.assignTo}/projects`;
-//     //         // default:
-//     //         // return this._super(...arguments);
-//     //     // }
-//     // }
-	
-// 	// findRecord(store, type, snapshot) {
-//     //     let data = this.serialize(snapshot);
-
-//     // session: service('session'),
-
-// 	// buildURL(modelName, id, snapshot, requestType, query){
-//     //     let memberId = this.get('session').session.content.authenticated.userdata.id;
-//     //     console.log(memberId,"aaaadsdnj")
-//     //     return `http://localhost:8000/api/v1/member/${memberId}/projects/`;
-//     // }
-
 import DS from 'ember-data';
 import {inject as service} from '@ember/service';
+
+import Em from 'ember';
 
 
 export default DS.RESTAdapter.extend({
 	//making a server call to projects
 	session: service('session'),
     buildURL(modelName, id, snapshot, requestType, query){
-        // if (query) {
-        //     console.log(query.assignTo,"gggg")
-        //     return  `http://localhost:8000/api/v1/teams/${query.assignTo.teamId}/projects`;
-        // }else {
-            // let a = this.get('session');
-            // console.log(a.content,"hgfchgdasghsagh")
-            // let memberId =this.get('session').session.content.authenticated.userdata.id;
-            return `http://localhost:8000/api/v1/member/345/projects/`;
-        // }
+        if (query) {
+            console.log(query.assignTo,"gggg")
+            return  `http://localhost:8000/api/v1/teams/${query.assignTo.teamId}/projects`;
+        }else {
+            let memberId =this.get('session').session.content.authenticated.userdata.id;
+            console.log(memberId,"hgfchgdasghsagh")
+            return `http://localhost:8000/api/v1/member/${memberId}/projects/`;
+        }
         debugger
     },
     urlForQuery (query, modelName) {
@@ -66,25 +27,4 @@ export default DS.RESTAdapter.extend({
             // return this._super(...arguments);
         // }
     }
-	
-	// findRecord(store, type, snapshot) {
-    //     let data = this.serialize(snapshot);
-
-    //     return new Promise((resolve) => {
-    //         Em.$.ajax({
-    //             async: true,
-    //             crossDomain: true,
-    //             type: 'GET',
-    //             contentType: 'application/json',
-    //             data: JSON.stringify(data),
-    //             url: `http://172.23.238.243:4000/api/teams/team1`,
-    //             success: {
-    //                 200: ()=>{
-    //                     Em.run(null, resolve);
-    //                 }
-    //             }
-    //         })
-    //     })
-    // }
-
 })
